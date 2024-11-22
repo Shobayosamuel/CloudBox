6a4e277a1406f04778df0e7e5dad16ec641a474e
shobayo samuel <shobayosamuel62@gmail.com>
2024-11-22 17:40:24 +0100

    fix: cors fix

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
     // CORS
     r.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"http://localhost:3000", "https://cloudbox-seven.vercel.app/"},
+        AllowOrigins:     []string{"*"},
         AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
         AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization", "Refresh-Token"},
         ExposeHeaders:    []string{"Content-Length"},
